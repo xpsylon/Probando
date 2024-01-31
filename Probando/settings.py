a6f71944645c7bab8b26d136421bf6e6d66a4e04
xpsylon <xxxpsylon@gmail.com>
2024-01-31 15:02:59 +0100

    migration to postgres

--- Probando/settings.py
+++ Probando/settings.py
@@ -75,10 +75,21 @@ WSGI_APPLICATION = 'Probando.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'probando',
+        'USER': 'postgres',
+        'PASSWORD': 'bananas014', 
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
