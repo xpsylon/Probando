6eddcfc37d0a63f7e61eaa2040402dfd1503cc91
xpsylon <xxxpsylon@gmail.com>
2024-03-10 18:18:04 +0100

    lo de pep munar

--- Probando/urls.py
+++ Probando/urls.py
@@ -23,5 +23,5 @@ from django.views.generic import TemplateView
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('django.contrib.auth.urls')),
-    path('temp/', TemplateView.as_view(template_name='pruebas:temp.html', extra_content={'title', 'Prueba numero 7'})),
+    path('temp/', TemplateView.as_view(template_name='pruebas:temp.html', extra_content={'title:Proyecto Siete'})),
 ]
