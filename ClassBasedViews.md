1bab1087446fee32c8bd11f155a84a409c85bc01
xpsylon <xxxpsylon@gmail.com>
2024-03-12 19:16:29 +0100

    testeando como un campeon

---
+++ ClassBasedViews.md
@@ -0,0 +1,8 @@
+#CLASS BASED VIEWS
+
+##CLASS BASED GENERIC VIEWS
+1. TEMPLATE VIEW
+    django.views.generic
+    For static template purposes
+2. REDIRECT VIEW
+    django.views.generic       
--- pruebas/urls.py
+++ pruebas/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
-from django.views.generic import TemplateView
+from django.views.generic import TemplateView, RedirectView
 from .views import Temp2View
 
 app_name='pruebas'
 
 urlpatterns = [
     path('temp/', TemplateView.as_view(template_name='pruebas/temp.html', extra_context={'title':'Proyecto Siete'})),
-    path('lista/', Temp2View.as_view(), name='temp2')
-]
+    path('lista/', Temp2View.as_view(), name='temp2'), #View BASED on Template Views. Check View file.
+    path('redt/', RedirectView.as_view())
+]   
